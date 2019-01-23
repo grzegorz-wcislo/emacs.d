4cba9a961a61cceabd179bacc064901f7d60f400
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2019-01-23 17:43:54 +0100

    Switch to literate configuration

--- init.el
+++ init.el
@@ -1,19 +1,21 @@
+;; Add package archives
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(package-initialize)
+
+;; Ensure that use-package is installed.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Load literate configuration
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
+;; Load custom configuartion
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
 
-(add-to-list 'load-path (expand-file-name "init" user-emacs-directory))
-
-(setq make-backup-files nil)
-
-(require 'init-packages)
-(require 'init-languages)
-(require 'init-interface)
-(require 'init-editor)
-(require 'init-keybindings)
-(require 'init-evil)
-(require 'init-git)
-(require 'init-scratch)
-
 (server-start)
-
-(provide 'init)
